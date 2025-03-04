88724068b241d5bdd58e25af991fad6f57422cd3
FerasFSD <ferasdabour92@gmail.com>
2025-03-04 18:21:01 +0100

    fix region api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 export const sendMessageToBot = async (token, message) => {
     try {
-      const response = await fetch('https://us-central1-chatbot-resume-bd8c0.cloudfunctions.net/chat', {
+      const response = await fetch(`https://europe-west1-chatbot-resume-bd8c0.cloudfunctions.net/chat`
+, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ token, message }),
